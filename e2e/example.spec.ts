0e9c99c953db8d082fb4757ed4e349004f660612
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-04 11:25:56 -0600

    fix: Add missing await for page load in example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,6 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/');
+  await page.waitForLoadState('domcontentloaded', { timeout: 15_000 });
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForLoadState('domcontentloaded', { timeout: 15_000 });
+  await expect(page.getByRole('heading', { name: /welcome back/i })).toBeVisible();
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);

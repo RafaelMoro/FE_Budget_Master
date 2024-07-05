1129f85d2ff53c1e9192ba29ccbc2dbb47e817aa
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-05 12:37:31 -0600

    Add fn validateMonthOlderRecords

--- src/utils/DateUtils/date.utils.ts
+++ src/utils/DateUtils/date.utils.ts
@@ -1,4 +1,7 @@
-import { ABBREVIATED_MONTHS, CompleteMonthsType, MONTHS } from '../../globalInterface';
+import {
+  ABBREVIATED_MONTHS, CompleteMonthsType, MONTHS,
+} from '../../globalInterface';
+import { ValidateMonthOlderRecordsProps } from './date.interface';
 
 export const updateAbbreviatedMonth = ({ newMonth }: { newMonth: CompleteMonthsType; }) => {
   const monthIndex = MONTHS.indexOf(newMonth);
@@ -7,3 +10,25 @@ export const updateAbbreviatedMonth = ({ newMonth }: { newMonth: CompleteMonthsT
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const sortByDate = (firstItem: any, secondItem: any) => +new Date(secondItem.date) - +new Date(firstItem.date);
+
+export const validateMonthOlderRecords = ({ month, year }: ValidateMonthOlderRecordsProps) => {
+  const now = new Date();
+  const currentMonth = now.getMonth(); // 0-based index (0 = January, 1 = February, ..., 11 = December)
+  const currentYear = now.getFullYear();
+
+  const monthFormatted = ABBREVIATED_MONTHS.indexOf(month);
+  const yearFormatted = Number(year);
+
+  const inputDate = new Date(yearFormatted, monthFormatted);
+
+  const isCurrentMonth = monthFormatted === currentMonth && yearFormatted === currentYear;
+  const isLastMonth = (monthFormatted === currentMonth - 1 && yearFormatted === currentYear)
+    || (currentMonth === 0 && monthFormatted === 12 && yearFormatted === currentYear - 1);
+  const isFutureMonth = inputDate > now;
+
+  return {
+    isCurrentMonth,
+    isLastMonth,
+    isFutureMonth,
+  };
+};

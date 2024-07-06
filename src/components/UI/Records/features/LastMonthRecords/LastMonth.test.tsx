72fe4b6f7de7691aec37e1329223d4a642e9995d
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-06 09:41:33 -0600

    Add test: Show last records expanded with loading state

--- src/components/UI/Records/features/LastMonthRecords/LastMonth.test.tsx
+++ src/components/UI/Records/features/LastMonthRecords/LastMonth.test.tsx
@@ -2,6 +2,7 @@ import { screen, within } from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 
 import { renderWithProviders } from '../../../../../tests/CustomWrapperRedux';
 import { LastMonthRecords } from './LastMonthRecords';
@@ -16,9 +17,9 @@ describe('Last Month Records', () => {
   });
 
   const history = createMemoryHistory();
+  const { lastMonthName } = getLastMonthDate();
 
   test('Show last month accordeon collapsed', () => {
-    const { lastMonthName } = getLastMonthDate();
     renderWithProviders(
       <Router location={history.location} navigator={history}>
         <LastMonthRecords color="red" accountId="some-account-id" isGuestUser={false} />
@@ -33,5 +34,28 @@ describe('Last Month Records', () => {
 
     expect(screen.getByText(`Last month: ${lastMonthName}`)).toBeInTheDocument();
     expect(icon).toBeInTheDocument();
+  });
+
+  test('Show last records expanded with loading state', async () => {
+    renderWithProviders(
+      <Router location={history.location} navigator={history}>
+        <LastMonthRecords color="blue" accountId="some-account-id" isGuestUser={false} />
+      </Router>,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    const accordion = screen.getByRole('button', {
+      name: `Last month: ${lastMonthName}`,
+    });
+
+    userEvent.click(accordion);
+
+    const totalExpenseText = await screen.findByText(/total expense:/i);
+    const totalIncomeText = screen.getByText(/total income:/i);
+    const loadingSkeletons = screen.getAllByTestId('record-loading-skeleton');
+
+    expect(totalExpenseText).toBeInTheDocument();
+    expect(totalIncomeText).toBeInTheDocument();
+    expect(loadingSkeletons).toHaveLength(3);
   });
 });

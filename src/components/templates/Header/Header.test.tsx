bc67ad0384b6e1364980ede7efe508215494b39f
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-20 00:44:11 -0600

    Add test: User clicks on hamburguer menu, click on Log in link and send him into Login page

--- src/components/templates/Header/Header.test.tsx
+++ src/components/templates/Header/Header.test.tsx
@@ -1,10 +1,12 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
+import userEvent from '@testing-library/user-event';
 import { Header } from './Header';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { getInitialUserInterfaceState, getUserMock } from '../../../tests/Global.mocks';
+import { LOGIN_ROUTE } from '../../../pages/RoutesConstants';
 
 describe('Header', () => {
   beforeEach(() => {
@@ -24,6 +26,28 @@ describe('Header', () => {
     expect(screen.getByRole('heading', { name: /budget master/i })).toBeInTheDocument();
     expect(screen.getByRole('img', { name: /budget master logo/i })).toBeInTheDocument();
     expect(screen.getByTestId('hamburguer-menu-header')).toBeInTheDocument();
+  });
+
+  test('User clicks on hamburguer menu, click on Log in link and send him into Login page', async () => {
+    const userInterfaceState = getInitialUserInterfaceState({ newWindowSize: 'Mobile' });
+    renderWithProviders(
+      <Router location={history.location} navigator={history}>
+        <Header isLandingPage />
+      </Router>,
+      { preloadedState: { userInterface: userInterfaceState } },
+    );
+
+    const hamburguerMenu = screen.getByTestId('hamburguer-menu-header');
+
+    expect(hamburguerMenu).toBeInTheDocument();
+    userEvent.click(hamburguerMenu);
+    expect(await screen.findByText(/log in/i)).toBeInTheDocument();
+    expect(screen.getByText(/register/i)).toBeInTheDocument();
+
+    userEvent.click(screen.getByText(/log in/i));
+    await waitFor(() => {
+      expect(history.location.pathname).toBe(LOGIN_ROUTE);
+    });
   });
 
   test('Show Header in Landing page for Destop', () => {

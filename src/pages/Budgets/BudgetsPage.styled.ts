5eb3032274616857d4fe5bfca60a751f89d58f67
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-20 17:53:53 -0600

    chore: Update BudgetsPage styling and add BudgetList section

--- src/pages/Budgets/BudgetsPage.styled.ts
+++ src/pages/Budgets/BudgetsPage.styled.ts
@@ -5,3 +5,12 @@ export const Main = styled.main`
   min-height: 100vh;
   background-color: ${AppColors.bgColorWhite};
 `;
+
+export const BudgetList = styled.section`
+  padding: 0 2rem;
+  width: 100%;
+  max-width: 155rem;
+  margin: 2rem auto 0 auto;
+  display: flex;
+  justify-content: center;
+`;
--- src/pages/Budgets/BudgetsPage.tsx
+++ src/pages/Budgets/BudgetsPage.tsx
@@ -1,17 +1,16 @@
 import { Typography } from '@mui/material';
 import { Header } from '../../components/templates';
 import { Budget } from '../../components/UI/Budget';
-import { Main } from './BudgetsPage.styled';
+import { BudgetList, Main } from './BudgetsPage.styled';
 import { currentBudget } from '../../components/UI/Budget/Budget.mocks';
 
 const BudgetsPage = () => (
   <Main>
     <Header />
     <Typography variant="h2" align="center">Budgets:</Typography>
-    { /* @TODO: Create Budget container */ }
-    <div style={{ marginTop: '2rem', padding: '0 2rem' }}>
+    <BudgetList>
       <Budget budget={currentBudget} />
-    </div>
+    </BudgetList>
   </Main>
 );
 

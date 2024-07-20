d8cf9690f8bec65d5f54eefec826770f09c9c172
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-20 00:30:15 -0600

    chore: Update Header tests and add landing page support

--- src/components/templates/Header/Header.test.tsx
+++ src/components/templates/Header/Header.test.tsx
@@ -7,12 +7,16 @@ import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { getInitialUserInterfaceState } from '../../../tests/Global.mocks';
 
 describe('Header', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
   const history = createMemoryHistory();
-  test('Show Header in Mobile', () => {
+  test('Show Header in Landing page for Mobile', () => {
     const userInterfaceState = getInitialUserInterfaceState({ newWindowSize: 'Mobile' });
     renderWithProviders(
       <Router location={history.location} navigator={history}>
-        <Header />
+        <Header isLandingPage />
       </Router>,
       { preloadedState: { userInterface: userInterfaceState } },
     );
@@ -20,7 +24,19 @@ describe('Header', () => {
     expect(screen.getByRole('heading', { name: /budget master/i })).toBeInTheDocument();
     expect(screen.getByRole('img', { name: /budget master logo/i })).toBeInTheDocument();
     expect(screen.getByTestId('hamburguer-menu-header')).toBeInTheDocument();
-    // expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
-    // expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
+  });
+
+  test('Show Header in Landing page for Destop', () => {
+    const userInterfaceState = getInitialUserInterfaceState({ newWindowSize: 'Desktop' });
+    renderWithProviders(
+      <Router location={history.location} navigator={history}>
+        <Header isLandingPage />
+      </Router>,
+      { preloadedState: { userInterface: userInterfaceState } },
+    );
+
+    expect(screen.queryByTestId('hamburguer-menu-header')).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
   });
 });

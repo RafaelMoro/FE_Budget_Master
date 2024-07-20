5dc98b833ff13da78c66a93f3c9d893b754b6a33
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-20 12:05:18 -0600

    Add header into Budgets page

--- src/pages/Budgets/Budgets.tsx
+++ src/pages/Budgets/Budgets.tsx
@@ -1,7 +1,11 @@
 import { Typography } from '@mui/material';
+import { Header } from '../../components/templates';
 
 const Budgets = () => (
-  <Typography variant="h1">Budgets</Typography>
+  <>
+    <Header />
+    <Typography variant="h1">Budgets</Typography>
+  </>
 );
 
 export { Budgets };

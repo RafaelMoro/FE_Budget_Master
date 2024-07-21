736a96a2247098f441d7f455952038c74d0118e3
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-21 08:41:22 -0600

    Add: Render data fetched from budget endpoint

--- src/components/UI/Budget/features/BudgetList/BudgetList.tsx
+++ src/components/UI/Budget/features/BudgetList/BudgetList.tsx
@@ -3,18 +3,27 @@ import { useAppSelector } from '../../../../../redux/hooks';
 import { useFetchBudgetsQuery } from '../../../../../redux/slices/Budgets/budgets.api';
 import { BudgetListContainer } from '../../../../../pages/Budgets/BudgetsPage.styled';
 import { Budget } from '../../Budget';
-import { currentBudget, periodicBudget } from '../../Budget.mocks';
+import { Error } from '../../../Error';
+import { ERROR_MESSAGE_GENERAL } from '../../../../../constants';
+import { HorizontalLoader } from '../../../HorizontalLoader';
 
 const BudgetList = () => {
   const user = useAppSelector((state) => state.user);
   const bearerToken = user.userInfo?.bearerToken as string;
-  const { data } = useFetchBudgetsQuery({ bearerToken }, { skip: !bearerToken });
-  console.log('data:', data);
+  const { data, isError, isLoading } = useFetchBudgetsQuery({ bearerToken }, { skip: !bearerToken });
+
+  if (isError) {
+    return (
+      <Error description={ERROR_MESSAGE_GENERAL} />
+    );
+  }
 
   return (
     <BudgetListContainer>
-      <Budget budget={currentBudget} />
-      <Budget budget={periodicBudget} />
+      { (isLoading) && <HorizontalLoader /> }
+      { (data && data.length > 0) && data.map((budget) => (
+        <Budget key={budget._id} budget={budget} />
+      )) }
     </BudgetListContainer>
   );
 };

496fbfdcce02cda63b87d96d08bd180600f7b7d1
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-20 19:17:42 -0600

    Update tests from budgets UI changes of removing description and chips

--- src/components/UI/Budget/Budget.test.tsx
+++ src/components/UI/Budget/Budget.test.tsx
@@ -9,19 +9,16 @@ describe('Budget', () => {
     renderWithProviders(<Budget budget={mockBudget} />);
 
     expect(screen.getByText('Fast food and beverages.')).toBeInTheDocument();
-    expect(screen.getByText('This budget is to control the amount of money spent in fast food')).toBeInTheDocument();
     expect(screen.getByText('Limit: $1,000.00')).toBeInTheDocument();
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
     expect(screen.getByText('$200.00')).toBeInTheDocument();
     expect(screen.getByText('20%')).toBeInTheDocument();
   });
 
-  test('Given a budget with large title and description, show budget with title and description truncated', () => {
+  test('Given a budget with large title and description, show budget with title truncated', () => {
     const mockBudget = getMockBudget({ hasLargeTitle: true, hasLargeDescription: true });
     renderWithProviders(<Budget budget={mockBudget} />);
 
     expect(screen.getByText('This is a very long title that should be truncated...')).toBeInTheDocument();
-    // eslint-disable-next-line max-len
-    expect(screen.getByText("This is a very long description that should be truncated on the budget when shown. Seems like it's still missing so...")).toBeInTheDocument();
   });
 });

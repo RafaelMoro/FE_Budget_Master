f3bdfa255d520a156b833c462045e7fe7de20066
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-21 09:40:19 -0600

    Add test: Show budget list with two loading skeletons while being on not mobile

--- src/components/UI/Budget/features/BudgetList/BudgetList.test.tsx
+++ src/components/UI/Budget/features/BudgetList/BudgetList.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
-import { userLoggedMock } from '../../../../../mocks/global.mocks';
 import { renderWithProviders } from '../../../../../tests/CustomWrapperRedux';
 import { BudgetList } from './BudgetList';
+import { getInitialUserInterfaceState, getUserMock } from '../../../../../tests/Global.mocks';
 
 describe('Budget List', () => {
   beforeEach(() => {
@@ -11,11 +11,24 @@ describe('Budget List', () => {
   });
 
   test('Show budget list with loading skeleton', () => {
+    const userMock = getUserMock({ isGuestUser: false });
     renderWithProviders(
       <BudgetList />,
-      { preloadedState: { user: userLoggedMock } },
+      { preloadedState: { user: userMock } },
     );
 
     expect(screen.getByTestId('budget-loading-skeleton')).toBeInTheDocument();
+  });
+
+  test('Show budget list with two loading skeletons while being on not mobile', () => {
+    const userMock = getUserMock({ isGuestUser: false });
+    const userInterfaceMock = getInitialUserInterfaceState({ newWindowSize: 'Desktop' });
+    renderWithProviders(
+      <BudgetList />,
+      { preloadedState: { user: userMock, userInterface: userInterfaceMock } },
+    );
+
+    const loadingSkeletons = screen.getAllByTestId('budget-loading-skeleton');
+    expect(loadingSkeletons).toHaveLength(2);
   });
 });

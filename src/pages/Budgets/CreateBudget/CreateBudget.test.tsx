b5cad43232fc546050b92448d1fe8c2a7de850e0
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-07-24 18:11:21 -0600

    Add suite test for create budget page

---
+++ src/pages/Budgets/CreateBudget/CreateBudget.test.tsx
@@ -0,0 +1,25 @@
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { screen } from '@testing-library/react';
+
+import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
+import { CreateBudget } from './CreateBudget';
+
+describe('Create budget page', () => {
+  const history = createMemoryHistory();
+  test('Should show the create budget page', () => {
+    renderWithProviders(
+      <Router location={history.location} navigator={history}>
+        <CreateBudget />
+      </Router>,
+    );
+
+    expect(screen.getByText('Create budget:')).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /name/i })).toBeInTheDocument();
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /budget limit/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /amount spent/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /next/i })).toBeInTheDocument();
+  });
+});

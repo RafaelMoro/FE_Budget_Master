3b474539ab2ed2533b6a9bef932b8013722ef0aa
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-07-24 18:24:30 -0600

    Add test Given a user clicking on next without entering any data, should show error validations'

--- src/components/UI/Budget/features/BudgetForm/BudgetForm.test.tsx
+++ src/components/UI/Budget/features/BudgetForm/BudgetForm.test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../../../../tests/CustomWrapperRedux';
 import { BudgetForm } from './BudgetForm';
 
@@ -23,5 +24,19 @@ describe('Budget form', () => {
     expect(screen.getByRole('textbox', { name: /amount spent/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /next/i })).toBeInTheDocument();
+  });
+
+  test('Given a user clicking on next without entering any data, should show error validations', async () => {
+    renderWithProviders(
+      <Router location={history.location} navigator={history}>
+        <BudgetForm />
+      </Router>,
+    );
+    const nextButton = screen.getByRole('button', { name: /next/i });
+    userEvent.click(nextButton);
+
+    expect(await screen.findByText(/budget name is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/budget limit is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/amount spent is required/i)).toBeInTheDocument();
   });
 });
--- src/validationsSchemas/budget.schema.ts
+++ src/validationsSchemas/budget.schema.ts
@@ -4,7 +4,7 @@ import { stringRequired } from './validations';
 const budgetValidation = {
   name: Yup.string().min(3).max(20).required('Budget name is required'),
   limit: stringRequired('Budget limit is required'),
-  currentAmount: stringRequired('Current amount is required'),
+  currentAmount: stringRequired('Amount spent is required'),
 };
 
 export const BudgetSchema = Yup.object(budgetValidation);

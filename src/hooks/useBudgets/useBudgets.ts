9067c4028734cf3515d49668d415af72301b7d47
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-26 12:46:00 -0600

    Remove notification functionality on use budgets as it shows an error message in screen instead of notifications

--- src/hooks/useBudgets/useBudgets.ts
+++ src/hooks/useBudgets/useBudgets.ts
@@ -1,17 +1,9 @@
-import { useNavigate } from 'react-router-dom';
-import { SystemStateEnum } from '../../enums';
-import { Actions, GeneralError } from '../../globalInterface';
-import { BUDGETS_ROUTE } from '../../pages/RoutesConstants';
-import { useNotification } from '../useNotification';
-import { ShowErrorNotificationBudgetProps } from './useBudgets.interface';
+import { GeneralError } from '../../globalInterface';
 import { useCreateBudgetMutation, useDeleteBudgetMutation, useEditBudgetMutation } from '../../redux/slices/Budgets/budgets.api';
 import { CreateBudgetValuesApiRequest, DeleteBudgetValues, EditBudgetValuesApiRequest } from '../../components/UI/Budget/Budget.interface';
 import { useAppSelector } from '../../redux/hooks';
 
 const useBudgets = () => {
-  const navigate = useNavigate();
-  const { updateGlobalNotification } = useNotification({});
-
   const [
     createBudgetMutation,
     { isLoading: isLoadingCreateBudget, isError: isErrorCreateBudget },
@@ -25,38 +17,13 @@ const useBudgets = () => {
   const userReduxState = useAppSelector((state) => state.user);
   const bearerToken = userReduxState.userInfo?.bearerToken as string;
 
-  const showErrorNotification = ({ errorMessage, action, goToBudgets = false }: ShowErrorNotificationBudgetProps) => {
-    const actions: Actions = {
-      create: 'creating',
-      edit: 'editing',
-    };
-    const verb = actions[action.toLowerCase() as keyof typeof actions];
-    updateGlobalNotification({
-      newTitle: 'Error',
-      newDescription: `Oops! An error ocurred while ${verb} your budget. Please try again later.`,
-      newStatus: SystemStateEnum.Error,
-    });
-    // eslint-disable-next-line no-console
-    console.error(`Error while submitting ${action} budget: ${errorMessage}`);
-
-    if (goToBudgets) {
-      // Navigate to dashboard
-      setTimeout(() => {
-        navigate(BUDGETS_ROUTE);
-      }, 3000);
-    }
-  };
-
   const createBudget = async ({ values }: { values: CreateBudgetValuesApiRequest }) => {
     try {
       await createBudgetMutation({ values, bearerToken });
     } catch (err) {
       const errorCatched = err as GeneralError;
-      showErrorNotification({
-        errorMessage: errorCatched?.data?.message ?? '',
-        action: 'Create',
-        goToBudgets: true,
-      });
+      // eslint-disable-next-line no-console
+      console.error('Error while creating budget: ', errorCatched?.data?.message);
     }
   };
 
@@ -65,11 +32,8 @@ const useBudgets = () => {
       await editBudgetMutation({ values, bearerToken });
     } catch (err) {
       const errorCatched = err as GeneralError;
-      showErrorNotification({
-        errorMessage: errorCatched?.data?.message ?? '',
-        action: 'Edit',
-        goToBudgets: true,
-      });
+      // eslint-disable-next-line no-console
+      console.error('Error while editing budget: ', errorCatched?.data?.message);
     }
   };
 

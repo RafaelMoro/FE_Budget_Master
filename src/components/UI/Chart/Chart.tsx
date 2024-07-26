d3c31870353f04ebf856fd1931059cbbdd41601b
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-25 23:18:00 -0600

    Add react chartjs 2

--- src/components/UI/Chart/Chart.tsx
+++ src/components/UI/Chart/Chart.tsx
@@ -1,5 +1,34 @@
-const Chart = () => (
-  <p>Chart</p>
-);
+/* eslint-disable import/no-extraneous-dependencies */
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+import { useAppSelector } from '../../../redux/hooks';
+import { AppColors } from '../../../styles';
+
+const Chart = () => {
+  const recordsState = useAppSelector((state) => state.records);
+  const recordsData = recordsState?.currentMonthRecordsData;
+  const data = (recordsData ?? []).map((record) => ({
+    date: record.fullDate,
+    amount: record.amount,
+  }));
+  const chartData = {
+    labels: data.map((record) => record.date),
+    datasets: [
+      {
+        label: 'Daily amount spending',
+        data: data.map((record) => record.amount),
+        backgroundColor: [
+          AppColors.primary,
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Bar data={chartData} />
+  );
+};
 
 export { Chart };

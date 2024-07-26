dfd105207617e71ebf93e48e6ceb459244dd6bb6
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-26 00:49:47 -0600

    Add util fn to process records on the top days the user has have more expenses

--- src/components/UI/Chart/Chart.tsx
+++ src/components/UI/Chart/Chart.tsx
@@ -2,22 +2,34 @@
 import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 
+import { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../redux/hooks';
 import { AppColors } from '../../../styles';
+import { getTop4DaysTotalAmount } from './Chart.util';
+
+interface Result {
+  date: string;
+  amount: number;
+}
 
 const Chart = () => {
   const recordsState = useAppSelector((state) => state.records);
   const recordsData = recordsState?.currentMonthRecordsData;
-  const data = (recordsData ?? []).map((record) => ({
-    date: record.fullDate,
-    amount: record.amount,
-  }));
+  const [data, setData] = useState<Result[]>([]);
+
+  useEffect(() => {
+    if (recordsData) {
+      const newData = getTop4DaysTotalAmount(recordsData);
+      setData(newData);
+    }
+  }, [recordsData]);
+
   const chartData = {
-    labels: data.map((record) => record.date),
+    labels: data.map((item) => item.date),
     datasets: [
       {
         label: 'Daily amount spending',
-        data: data.map((record) => record.amount),
+        data: data.map((item) => item.amount),
         backgroundColor: [
           AppColors.primary,
         ],
---
+++ src/components/UI/Chart/Chart.util.ts
@@ -0,0 +1,29 @@
+import { AnyRecord } from '../../../globalInterface';
+
+export function getTop4DaysTotalAmount(records: AnyRecord[]) {
+  const dayAmountMap = records.reduce<Record<string, number>>((acc, record) => {
+    const { fullDate, amount } = record;
+    if (!acc[fullDate]) {
+      acc[fullDate] = 0;
+    }
+    if (record?.isPaid) {
+      acc[fullDate] += amount;
+    }
+    return acc;
+  }, {});
+
+  // Filter out days with $0 amount
+  const dayAmountMapFiltered = Object.entries(dayAmountMap).filter((item) => {
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    const [day, amount] = item;
+    return amount > 0;
+  });
+
+  // Format the data to be used in the chart
+  const result = dayAmountMapFiltered.map((item) => ({
+    date: item[0],
+    amount: item[1],
+  }));
+
+  return result;
+}

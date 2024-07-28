f8762bcdcf451a17c7627aaf0b5a8666607c315b
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-27 22:48:22 -0600

    Update delete accounts be route

--- src/components/UI/Account/Account.constants.ts
+++ src/components/UI/Account/Account.constants.ts
@@ -1,3 +1,3 @@
 export const POST_PUT_ACCOUNT_ROUTE = '/account-actions';
-export const DELETE_ACCOUNT_ROUTE = 'accounts';
+export const DELETE_ACCOUNT_ROUTE = '/account-actions';
 export const GET_ACCOUNTS_ROUTE = '/account-actions';

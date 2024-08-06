0c0bfdacb468dc20c497dc8d3383259830ed3230
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-08-05 20:41:40 -0600

    Add styles to graphic card

--- src/components/templates/GraphicsCard/GraphicsCard.styled.ts
+++ src/components/templates/GraphicsCard/GraphicsCard.styled.ts
@@ -2,12 +2,15 @@ import styled from '@emotion/styled';
 import { AppColors } from '../../../styles';
 
 export const GraphicsCardContainer = styled.article`
+  width: 100%;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
   background-color: ${AppColors.white};
   box-shadow: 0 .4rem .8rem rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 1.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  place-items: center;
   gap: 2rem;
 `;

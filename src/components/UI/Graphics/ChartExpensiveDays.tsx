736b2b199d1894a3e302f7c8e138bbf9421d798a
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-16 14:42:21 -0600

    Add text no data to display in case chart expensive days has no data

--- src/components/UI/Graphics/ChartExpensiveDays.tsx
+++ src/components/UI/Graphics/ChartExpensiveDays.tsx
@@ -1,6 +1,7 @@
 import {
   BarChart, Bar, CartesianGrid, Tooltip, Legend, XAxis, YAxis,
 } from 'recharts';
+import { Typography } from '@mui/material';
 
 import { useAppSelector } from '../../../redux/hooks';
 import { ChartExpensiveDaysProps } from './Chart.interface';
@@ -19,15 +20,20 @@ const ChartExpensiveDays = ({ records }: ChartExpensiveDaysProps) => {
   }
 
   return (
-    <div data-testid="test-chart-something">
-      <BarChart width={500} height={300} data={expensiveDaysData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="amount" fill={AppColors.primary} />
-      </BarChart>
+    <div data-testid="chart-bar-expensive-days">
+      {expensiveDaysData.length === 0 && (
+        <Typography variant="body2">No data to display</Typography>
+      )}
+      { expensiveDaysData.length > 0 && (
+        <BarChart width={500} height={300} data={expensiveDaysData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="amount" fill={AppColors.primary} />
+        </BarChart>
+      )}
     </div>
   );
 };

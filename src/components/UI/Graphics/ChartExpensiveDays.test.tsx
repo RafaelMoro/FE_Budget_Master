9d6c728c14e5953f30c8048c0f2439523f9cf3fb
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-17 09:12:20 -0600

    Add test of expensive days chart for desktop

--- src/components/UI/Graphics/ChartExpensiveDays.test.tsx
+++ src/components/UI/Graphics/ChartExpensiveDays.test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { ChartExpensiveDays } from './ChartExpensiveDays';
 import { recordsChart } from './Chart.mocks';
+import { getInitialUserInterfaceState } from '../../../tests/Global.mocks';
 
 describe('ChartExpensiveDays chart component', () => {
   test('Show ChartExpensiveDays chart on mobile', () => {
@@ -9,5 +10,15 @@ describe('ChartExpensiveDays chart component', () => {
       <ChartExpensiveDays records={recordsChart} />,
     );
     expect(screen.getByTestId('chart-bar-expensive-days-mobile')).toBeInTheDocument();
+  });
+
+  test('Show ChartExpensiveDays chart on desktop', () => {
+    const userInterfaceState = getInitialUserInterfaceState({ newWindowSize: 'Desktop' });
+    renderWithProviders(
+      <ChartExpensiveDays records={recordsChart} />,
+      { preloadedState: { userInterface: userInterfaceState } },
+    );
+
+    expect(screen.getByTestId('chart-bar-expensive-days-desktop')).toBeInTheDocument();
   });
 });

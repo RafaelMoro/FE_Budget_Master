ead3e558aa19b295b8f8839ccc684010957de52e
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-08-17 17:46:08 -0600

    Remove card style from graphics card

--- src/components/templates/RecordsOverviewCard/RecordsOverviewCard.styled.ts
+++ src/components/templates/RecordsOverviewCard/RecordsOverviewCard.styled.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { AppColors, PrimaryButton, responsiveBreakpoints } from '../../../styles';
+import { PrimaryButton, responsiveBreakpoints } from '../../../styles';
 
 export const Container = styled.article`
   padding: 2rem 0;

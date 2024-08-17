423766015e054973c143007ea166ba6ebb6cf166
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-17 09:25:54 -0600

    Add test for categories chart

---
+++ src/components/UI/Graphics/ChartCategories.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
+import { recordsChart } from './Chart.mocks';
+import { ChartCategories } from './ChartCategories';
+
+describe('ChartCategories chart component', () => {
+  test('Show ChartCategories chart', () => {
+    renderWithProviders(
+      <ChartCategories records={recordsChart} />,
+    );
+
+    expect(screen.getByTestId('categories-chart')).toBeInTheDocument();
+  });
+});

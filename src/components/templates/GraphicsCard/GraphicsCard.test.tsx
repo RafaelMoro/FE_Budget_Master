9ea1aa986881ab12ad7300857fe82b155b7733bc
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-17 09:47:47 -0600

    Add test into graphics card

--- src/components/templates/GraphicsCard/GraphicsCard.test.tsx
+++ src/components/templates/GraphicsCard/GraphicsCard.test.tsx
@@ -4,6 +4,15 @@ import { recordsChart } from '../../UI/Graphics/Chart.mocks';
 import { GraphicsCard } from './GraphicsCard';
 
 describe('GraphicsCard', () => {
+  test('Show Graphics Card without data', () => {
+    renderWithProviders(
+      <GraphicsCard records={[]} />,
+    );
+
+    expect(screen.getByText('Useful statistics of your finances')).toBeInTheDocument();
+    expect(screen.getByText('No records created yet. Start creating records this month to start showing statistics.')).toBeInTheDocument();
+  });
+
   test('Show Graphics Card with data', () => {
     renderWithProviders(
       <GraphicsCard records={recordsChart} />,

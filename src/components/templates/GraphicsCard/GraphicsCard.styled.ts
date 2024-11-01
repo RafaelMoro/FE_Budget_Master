13143fe3fb96934659ef5924d695f686fea7d18f
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-10-31 23:01:43 -0600

    Remove box shador from graphics card

--- src/components/templates/GraphicsCard/GraphicsCard.styled.ts
+++ src/components/templates/GraphicsCard/GraphicsCard.styled.ts
@@ -9,7 +9,6 @@ export const GraphicsCardContainer = styled.article`
   padding: 2rem 0;
   margin-bottom: ${appTheme.spacing(4)};
   background-color: ${AppColors.white};
-  box-shadow: 0 .4rem .8rem rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 1.5rem;
   display: grid;

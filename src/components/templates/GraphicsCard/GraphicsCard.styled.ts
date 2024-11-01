68b224d97623a9c85d8d5197aaebe92a5c8afcb5
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-10-31 22:37:21 -0600

    Add margin bottom to the graphics card

--- src/components/templates/GraphicsCard/GraphicsCard.styled.ts
+++ src/components/templates/GraphicsCard/GraphicsCard.styled.ts
@@ -2,11 +2,16 @@ import styled from '@emotion/styled';
 import { Typography } from '@mui/material';
 import { TextWithMarginBottomProps } from './GraphicsCard.interface';
 import { appTheme } from '../../../styles/theme';
+import { AppColors } from '../../../styles';
 
 export const GraphicsCardContainer = styled.article`
   width: 100%;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
+  padding: 2rem 0;
+  margin-bottom: ${appTheme.spacing(4)};
+  background-color: ${AppColors.white};
+  box-shadow: 0 .4rem .8rem rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 1.5rem;
   display: grid;
   grid-template-columns: 1fr;
   place-items: center;

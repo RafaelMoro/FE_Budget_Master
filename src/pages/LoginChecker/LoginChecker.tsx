baaa67f755afab66eb1621d963d5fca6536888cc
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-12-25 10:22:22 -0600

    Add login checker

---
+++ src/pages/LoginChecker/LoginChecker.tsx
@@ -0,0 +1,12 @@
+const LoginChecker = () => {
+  const checkLogin = () => {};
+
+  return (
+    <main>
+      <h1>Bienvenido a Budget Master</h1>
+      <p>Revisando si ya ha iniciado sesión...</p>
+    </main>
+  );
+};
+
+export { LoginChecker };
---
+++ src/pages/LoginChecker/index.ts
@@ -0,0 +1,1 @@
+export { LoginChecker } from './LoginChecker';

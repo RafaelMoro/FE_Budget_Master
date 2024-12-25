f8c56abfe0b532f1dd88aa31207072e6189c68a7
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-12-25 08:53:09 -0600

    add try app images into try app page

--- src/pages/TryAppPage/TryApp.tsx
+++ src/pages/TryAppPage/TryApp.tsx
@@ -8,6 +8,8 @@ import { PrimaryButton, SecondaryButton } from '../../styles';
 import {
   ButtonContainer, Description, LogoTitleLogin, Main,
 } from './TryApp.styled';
+import tryAppEmptyAccountImage from '../../assets/try-app/try-app-empty-account.webp';
+import tryAppWithDataImage from '../../assets/try-app/try-app-with-data.webp';
 
 const TryApp = () => {
   const navigate = useNavigate();
@@ -30,7 +32,9 @@ const TryApp = () => {
       </Typography>
       <Description>Para probar la aplicación podemos cargar datos de prueba o puedes decidir crear todos los datos por ti mismo.</Description>
       <ButtonContainer>
+        <img src={tryAppEmptyAccountImage} alt="Budget Master App with no data screenshot" />
         <SecondaryButton type="button" onClick={handleClickSampleData}>Usar datos de muestra</SecondaryButton>
+        <img src={tryAppWithDataImage} alt="Budget Master App with data screenshot" />
         <PrimaryButton type="button" onClick={handleClickSampleData}>Usar mis propios datos</PrimaryButton>
       </ButtonContainer>
     </Main>

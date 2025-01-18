a77216ef5155e75efe57c63a2823461211ec78bb
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-01-18 12:44:40 -0600

    fix reset password tests

--- src/pages/LoginModule/ResetPassword/ResetPassword.test.tsx
+++ src/pages/LoginModule/ResetPassword/ResetPassword.test.tsx
@@ -236,9 +236,9 @@ describe('Reset password page', () => {
       );
 
       const password = 'MiContraseña2022!';
-      passwordInput = screen.getByLabelText(/new password/i);
-      confirmPasswordInput = screen.getByLabelText(/confirm password/i);
-      resetPasswordButton = screen.getByRole('button', { name: /reset password/i });
+      passwordInput = screen.getByLabelText(/nueva contraseña/i);
+      confirmPasswordInput = screen.getByLabelText(/confirmar contraseña/i);
+      resetPasswordButton = screen.getByRole('button', { name: /cambiar/i });
 
       userEvent.type(passwordInput, password);
       userEvent.type(confirmPasswordInput, password);

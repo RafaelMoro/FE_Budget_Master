1088ea566284aec6e7085207898222e4fb8e0890
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-01-24 23:50:40 -0600

    Translate NoRecordsFound

--- src/components/UI/Records/features/NoRecordsFound/NoRecordsFound.tsx
+++ src/components/UI/Records/features/NoRecordsFound/NoRecordsFound.tsx
@@ -18,9 +18,9 @@ const NoRecordsFound = () => {
         <img src={noRecordsFoundImgPng} alt="No Records Found" />
       </NotRecordsFoundPicture>
       <Typography align="center" variant="body2">
-        You have not created records for this month.
+        No has creado transacciones para este mes.
       </Typography>
-      <PrimaryButton onClick={navigateToCreateRecord}>Create Record</PrimaryButton>
+      <PrimaryButton onClick={navigateToCreateRecord}>Crear registro</PrimaryButton>
     </NoRecordsFoundContainer>
   );
 };

62e41ff3b9725499539e38bc77b73cc151be5181
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-01-30 11:02:18 -0600

    fix test expense template

--- src/components/UI/Records/features/ExpenseTemplate/ExpenseTemplate.test.tsx
+++ src/components/UI/Records/features/ExpenseTemplate/ExpenseTemplate.test.tsx
@@ -14,7 +14,7 @@ describe('<ExpenseTemplate />', () => {
   const history = createMemoryHistory();
 
   let createRecordButton: HTMLElement | null = null;
-  test('Show Expense Template with title, description, amount, tags and button', () => {
+  test.only('Show Expense Template with title, description, amount, tags and button', () => {
     renderWithProviders(
       <Router location={history.location} navigator={history}>
         <ExpenseTemplate edit={false} typeOfRecord="expense" />
@@ -25,18 +25,18 @@ describe('<ExpenseTemplate />', () => {
     const budgetCombobox = screen.getByTestId('select-record-budget');
 
     expect(screen.getByRole('textbox', { name: /Cantidad disponible/i })).toBeInTheDocument();
-    expect(screen.getByRole('textbox', { name: /date and time/i })).toBeInTheDocument();
-    expect(screen.getByRole('textbox', { name: /short description/i })).toBeInTheDocument();
-    expect(screen.getByRole('textbox', { name: /description \(optional\)/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /fecha y hora/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /pequeña descripción/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /descripción \(opcional\)/i })).toBeInTheDocument();
     expect(categoryCombobox).toBeInTheDocument();
     expect(subCategoryCombobox).toBeInTheDocument();
-    expect(screen.getByRole('textbox', { name: /tag \(optional\)/i })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: /etiqueta \(opcional\)/i })).toBeInTheDocument();
     expect(budgetCombobox).toBeInTheDocument();
-    expect(screen.getByText(/Note:/i)).toBeInTheDocument();
-    expect(screen.getByText(/people related to this transaction: 0/i)).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /add person/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /create record/i })).toBeInTheDocument();
+    expect(screen.getByText(/Nota:/i)).toBeInTheDocument();
+    expect(screen.getByText(/personas relacionadas a esta transacción: 0/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /agregar persona/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /cancelar/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /crear gasto/i })).toBeInTheDocument();
   });
 
   test('Given an credit account, show the transaction paid input', () => {

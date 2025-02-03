85fdf74dcc8f2a5c04090ee26a4ec2e33624230e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-02 22:15:04 -0600

    adjust styles to date picker value

--- src/components/UI/DatePickerValue/DatePickerValue.tsx
+++ src/components/UI/DatePickerValue/DatePickerValue.tsx
@@ -1,8 +1,9 @@
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers';
+import { DatePicker, pickersLayoutClasses } from '@mui/x-date-pickers';
 
 import { SelectFormikFieldProps } from '../../../globalInterface';
+import { globalConfiguration } from '../../../styles';
 
 interface DatePickerValueProps {
   label: string;
@@ -19,6 +20,45 @@ const DatePickerValue = ({ label, field, setFieldValueCb }: DatePickerValueProps
         label={label}
         value={value}
         onChange={(newValue) => setFieldValueCb(name, newValue)}
+        slotProps={{
+          layout: {
+            sx: {
+              [`& .${pickersLayoutClasses.toolbar} > span`]: {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              '& .MuiDateTimePickerTabs-root .MuiSvgIcon-root': {
+                fontSize: '2rem',
+              },
+              // Time picker hour size
+              '& .MuiTimeClock-root': {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              // Days of the week
+              '& .MuiDayCalendar-header > span': {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              // Button to change year.
+              '& .MuiPickersYear-root > button': {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              '& .MuiDateTimePickerToolbar-dateContainer > button > span': {
+                fontSize: globalConfiguration.mobile.fontSizes.H4,
+              },
+              '& .MuiDateTimePickerToolbar-timeContainer .MuiPickersToolbarText-root': {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              '& .MuiDateTimePickerToolbar-timeContainer .MuiDateTimePickerToolbar-separator': {
+                fontSize: globalConfiguration.mobile.fontSizes.P,
+              },
+              '& .MuiDateTimePickerToolbar-timeDigitsContainer': {
+                display: 'grid',
+                gridTemplateColumns: '1fr 1fr 1fr',
+                gridTemplateRows: '1fr',
+                placeItems: 'center',
+              },
+            },
+          },
+        }}
       />
     </LocalizationProvider>
   );

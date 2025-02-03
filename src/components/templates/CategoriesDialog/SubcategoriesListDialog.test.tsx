be228bfdd20f91726138c7f55cc534492d413a05
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 12:56:55 -0600

    add buttons edit and delete to test in subcategories list dialog

--- src/components/templates/CategoriesDialog/SubcategoriesListDialog.test.tsx
+++ src/components/templates/CategoriesDialog/SubcategoriesListDialog.test.tsx
@@ -7,6 +7,8 @@ describe('<SubcategoriesListDialog />', () => {
     const subcategories = ['First subcategory', 'Second subcategory'];
     render(<SubcategoriesListDialog categoryName={categoryName} subCategories={subcategories} openList />);
 
+    expect(screen.getByRole('button', { name: /boton-editar-categoria-First category/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /boton-eliminar-categoria-First category/i })).toBeInTheDocument();
     expect(screen.getByText(/first subcategory/i)).toBeInTheDocument();
     expect(screen.getByText(/second subcategory/i)).toBeInTheDocument();
   });

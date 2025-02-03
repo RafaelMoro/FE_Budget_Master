350b16302f484735595e98586e51759eb4a65ac6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 12:42:04 -0600

    add delete and edit icons into categories dialog list

--- src/components/templates/CategoriesDialog/CategoriesListDialog.tsx
+++ src/components/templates/CategoriesDialog/CategoriesListDialog.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import {
-  ListItemButton, ListItemText,
+  ListItemButton, ListItemText, IconButton,
 } from '@mui/material';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
 import { SubcategoriesListDialog } from './SubcategoriesListDialog';
 import { ICON_SIZE } from '../../../constants';
+import { AppIcon } from '../../UI/Icons';
+import { AppColors, FlexContainer } from '../../../styles';
 
 interface CategoriesListDialogProps {
   categoryName: string;
@@ -19,12 +21,23 @@ const CategoriesListDialog = ({ categoryName, subCategories }: CategoriesListDia
     setOpenList((prevState) => !prevState);
   };
 
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+
   return (
     <>
       <ListItemButton onClick={handleClick}>
         <ListItemText primary={categoryName} />
         {openList ? <ExpandLess sx={ICON_SIZE} /> : <ExpandMore sx={ICON_SIZE} />}
       </ListItemButton>
+      <FlexContainer gap={2} justifyContent="center">
+        <IconButton aria-label={`boton-editar-categoria-${categoryName}`} onClick={handleEdit}>
+          <AppIcon icon="Edit" fillColor={AppColors.primary} />
+        </IconButton>
+        <IconButton aria-label={`boton-eliminar-categoria-${categoryName}`} onClick={handleDelete}>
+          <AppIcon icon="Delete" fillColor={AppColors.negative} />
+        </IconButton>
+      </FlexContainer>
       <SubcategoriesListDialog subCategories={subCategories} openList={openList} />
     </>
 

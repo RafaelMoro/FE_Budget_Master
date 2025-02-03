38e14ff85bf231be9c730df9bf2e9188ca1219d9
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 14:40:00 -0600

    add more style to category dialog

--- src/components/templates/CategoriesDialog/CategoriesDialog.styled.ts
+++ src/components/templates/CategoriesDialog/CategoriesDialog.styled.ts
@@ -1,9 +1,20 @@
 import styled from '@emotion/styled';
 import {
-  ListItemText,
+  ListItemText, IconButton,
 } from '@mui/material';
 import { appTheme } from '../../../styles/theme';
 import { AppColors } from '../../../styles';
+
+export const CategoriesDialogContainer = styled.div`
+  display: grid;
+  padding: ${appTheme.spacing(3)};
+  grid-template-columns: 1fr;
+  gap: ${appTheme.spacing(3)};
+`;
+
+export const CloseIconButton = styled(IconButton)`
+  justify-self: end;
+`;
 
 export const SubcategoryText = styled(ListItemText)`
   padding: ${appTheme.spacing(2)};
--- src/components/templates/CategoriesDialog/CategoriesDialog.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import {
-  Dialog, IconButton, Typography, List,
+  Dialog, Typography, List,
 } from '@mui/material';
 
 import { useCategories } from '../../../hooks';
 import { CategoryUI } from '../../../globalInterface';
 import { AppIcon } from '../../UI/Icons';
 import { CategoriesListDialog } from './CategoriesListDialog';
+import { CloseIconButton, CategoriesDialogContainer } from './CategoriesDialog.styled';
 
 interface CategoriesModalProps {
   open: boolean
@@ -32,31 +33,38 @@ const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => {
 
   return (
     <Dialog onClose={onClose} open={open}>
-      <IconButton onClick={onClose}>
-        <AppIcon icon="Close" />
-      </IconButton>
-      <Typography variant="h4" align="center">Categorías</Typography>
-      <List
-        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-        component="nav"
-        aria-labelledby="nested-list-categories"
-      >
-        {
-          (categories.length === 0 && isFetching) && (
-            <Typography>Cargando categorías</Typography>
-          )
-        }
-        {
-          (categories.length === 0 && isError) && (
-            <Typography>Error al cargar categorías</Typography>
-          )
-        }
-        {
-          (categories.length > 0) && categories.map(({ category, subcategories, categoryId }) => (
-            <CategoriesListDialog key={categoryId} subCategories={subcategories} categoryName={category} />
-          ))
-        }
-      </List>
+      <CategoriesDialogContainer>
+        <CloseIconButton onClick={onClose}>
+          <AppIcon icon="Close" />
+        </CloseIconButton>
+        <Typography variant="h4" align="center">Categorías</Typography>
+        <Typography>
+          Haga click en cualquier categoría para ver sus subcategorías. Dentro encontrará los botones para editar o eliminar esa categoría.
+        </Typography>
+        <List
+          sx={{
+            width: '100%', maxWidth: 360, bgcolor: 'background.paper', justifySelf: 'center',
+          }}
+          component="nav"
+          aria-labelledby="nested-list-categories"
+        >
+          {
+            (categories.length === 0 && isFetching) && (
+              <Typography>Cargando categorías</Typography>
+            )
+          }
+          {
+            (categories.length === 0 && isError) && (
+              <Typography>Error al cargar categorías</Typography>
+            )
+          }
+          {
+            (categories.length > 0) && categories.map(({ category, subcategories, categoryId }) => (
+              <CategoriesListDialog key={categoryId} subCategories={subcategories} categoryName={category} />
+            ))
+          }
+        </List>
+      </CategoriesDialogContainer>
     </Dialog>
   );
 };

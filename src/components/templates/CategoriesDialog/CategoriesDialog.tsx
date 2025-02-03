aa44c044b42b75c6219d542cf7ce006b4742ceac
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 11:29:49 -0600

    add list into categories dialog

--- src/components/templates/CategoriesDialog/CategoriesDialog.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.tsx
@@ -1,4 +1,8 @@
-import { Dialog, IconButton, Typography } from '@mui/material';
+import { useState } from 'react';
+import {
+  Dialog, IconButton, Typography, List, ListItemButton, ListItemText, Collapse,
+} from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { AppIcon } from '../../UI/Icons';
 
 interface CategoriesModalProps {
@@ -7,7 +11,11 @@ interface CategoriesModalProps {
 }
 
 const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => {
-  const something = '';
+  const [openList, setOpenList] = useState(true);
+
+  const handleClick = () => {
+    setOpenList((prevState) => !prevState);
+  };
 
   return (
     <Dialog onClose={onClose} open={open}>
@@ -15,6 +23,29 @@ const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => {
         <AppIcon icon="Close" />
       </IconButton>
       <Typography>Categorías</Typography>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-categories"
+      >
+        <ListItemButton>
+          <ListItemText primary="Primera categoria" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText primary="Segunda categoria" />
+        </ListItemButton>
+        <ListItemButton onClick={handleClick}>
+          <ListItemText primary="Tercera categoria" />
+          {openList ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={openList} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemText primary="Subcategoria 1" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </List>
     </Dialog>
   );
 };

b26f301785ae8f49e78091ccf6f17b7786b096ae
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 11:50:40 -0600

    add categories list dialog

--- src/components/templates/CategoriesDialog/CategoriesDialog.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.tsx
@@ -1,48 +1,34 @@
-import { useState } from 'react';
 import {
   Dialog, IconButton, Typography, List, ListItemButton, ListItemText,
 } from '@mui/material';
-import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { AppIcon } from '../../UI/Icons';
-import { SubcategoriesListDialog } from './SubcategoriesListDialog';
+import { CategoriesListDialog } from './CategoriesListDialog';
 
 interface CategoriesModalProps {
   open: boolean
   onClose: () => void
 }
 
-const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => {
-  const [openList, setOpenList] = useState(true);
-
-  const handleClick = () => {
-    setOpenList((prevState) => !prevState);
-  };
-
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <IconButton onClick={onClose}>
-        <AppIcon icon="Close" />
-      </IconButton>
-      <Typography>Categorías</Typography>
-      <List
-        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-        component="nav"
-        aria-labelledby="nested-list-categories"
-      >
-        <ListItemButton>
-          <ListItemText primary="Primera categoria" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemText primary="Segunda categoria" />
-        </ListItemButton>
-        <ListItemButton onClick={handleClick}>
-          <ListItemText primary="Tercera categoria" />
-          {openList ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-        <SubcategoriesListDialog openList={openList} />
-      </List>
-    </Dialog>
-  );
-};
+const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => (
+  <Dialog onClose={onClose} open={open}>
+    <IconButton onClick={onClose}>
+      <AppIcon icon="Close" />
+    </IconButton>
+    <Typography>Categorías</Typography>
+    <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      component="nav"
+      aria-labelledby="nested-list-categories"
+    >
+      <ListItemButton>
+        <ListItemText primary="Primera categoria" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText primary="Segunda categoria" />
+      </ListItemButton>
+      <CategoriesListDialog categoryName="Tercera categoria" />
+    </List>
+  </Dialog>
+);
 
 export { CategoriesDialog };
---
+++ src/components/templates/CategoriesDialog/CategoriesListDialog.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import {
+  ListItemButton, ListItemText,
+} from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+
+import { SubcategoriesListDialog } from './SubcategoriesListDialog';
+
+const CategoriesListDialog = ({ categoryName }: { categoryName: string }) => {
+  const [openList, setOpenList] = useState(true);
+
+  const handleClick = () => {
+    setOpenList((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <ListItemButton onClick={handleClick}>
+        <ListItemText primary={categoryName} />
+        {openList ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <SubcategoriesListDialog openList={openList} />
+    </>
+
+  );
+};
+
+export { CategoriesListDialog };

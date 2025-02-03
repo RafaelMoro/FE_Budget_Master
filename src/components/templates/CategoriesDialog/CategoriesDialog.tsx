dc1309a8bdb4949d7a98990918023142812fce95
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 14:04:40 -0600

    add fetching of categories in categories dialog

--- src/components/templates/CategoriesDialog/CategoriesDialog.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.tsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from 'react';
 import {
   Dialog, IconButton, Typography, List,
 } from '@mui/material';
+
+import { useCategories } from '../../../hooks';
+import { CategoryUI } from '../../../globalInterface';
 import { AppIcon } from '../../UI/Icons';
 import { CategoriesListDialog } from './CategoriesListDialog';
 
@@ -9,24 +13,52 @@ interface CategoriesModalProps {
   onClose: () => void
 }
 
-const subcategories = ['Primera subcategoría', 'Segunda subcategoría', 'Tercera subcategoría'];
+const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => {
+  const {
+    currentData, isError, isFetching, isSuccess,
+  } = useCategories();
+  const [categories, setCategories] = useState<CategoryUI[]>([]);
 
-const CategoriesDialog = ({ open, onClose }: CategoriesModalProps) => (
-  <Dialog onClose={onClose} open={open}>
-    <IconButton onClick={onClose}>
-      <AppIcon icon="Close" />
-    </IconButton>
-    <Typography>Categorías</Typography>
-    <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-categories"
-    >
-      <CategoriesListDialog subCategories={subcategories} categoryName="Primera categoria" />
-      <CategoriesListDialog subCategories={subcategories} categoryName="Segunda categoria" />
-      <CategoriesListDialog subCategories={subcategories} categoryName="Tercera categoria" />
-    </List>
-  </Dialog>
-);
+  useEffect(() => {
+    if (isSuccess && currentData) {
+      const newCategories: CategoryUI[] = currentData?.map((category) => ({
+        category: category.categoryName,
+        categoryId: category._id,
+        subcategories: category.subCategories,
+      }));
+      setCategories(newCategories);
+    }
+  }, [currentData, isSuccess]);
+
+  return (
+    <Dialog onClose={onClose} open={open}>
+      <IconButton onClick={onClose}>
+        <AppIcon icon="Close" />
+      </IconButton>
+      <Typography>Categorías</Typography>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-categories"
+      >
+        {
+          (categories.length === 0 && isFetching) && (
+            <Typography>Cargando categorías</Typography>
+          )
+        }
+        {
+          (categories.length === 0 && isError) && (
+            <Typography>Error al cargar categorías</Typography>
+          )
+        }
+        {
+          (categories.length > 0) && categories.map(({ category, subcategories, categoryId }) => (
+            <CategoriesListDialog key={categoryId} subCategories={subcategories} categoryName={category} />
+          ))
+        }
+      </List>
+    </Dialog>
+  );
+};
 
 export { CategoriesDialog };

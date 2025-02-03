1b00a30f1bf9965b36af9869c891efe1141152cd
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 10:40:59 -0600

    add Click on avatar and open menu test

--- src/components/templates/HeaderAvatarConfig/HeaderAvatarConfig.test.tsx
+++ src/components/templates/HeaderAvatarConfig/HeaderAvatarConfig.test.tsx
@@ -1,4 +1,6 @@
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { HeaderAvatarConfig } from './HeaderAvatarConfig';
 
 describe('<HeaderAvatarConfig />', () => {
@@ -6,5 +8,16 @@ describe('<HeaderAvatarConfig />', () => {
     const signOut = jest.fn();
     render(<HeaderAvatarConfig initials="JD" signOut={signOut} />);
     screen.getByText(/jd/i);
+  });
+
+  test('Click on avatar and open menu', async () => {
+    const signOut = jest.fn();
+    render(<HeaderAvatarConfig initials="JD" signOut={signOut} />);
+
+    const button = screen.getByRole('button', { name: /open-configuration-button/i });
+    userEvent.click(button);
+
+    expect(await screen.findByRole('menuitem', { name: /categorías/i })).toBeInTheDocument();
+    expect(screen.getByRole('menuitem', { name: /cerrar sesión/i })).toBeInTheDocument();
   });
 });

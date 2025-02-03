891baa0769ddfaea9b6e48c871e031fb69b77187
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-03 12:34:16 -0600

    add test Click on category, then show subcategories list

--- src/components/templates/CategoriesDialog/CategoriesListDialog.test.tsx
+++ src/components/templates/CategoriesDialog/CategoriesListDialog.test.tsx
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { CategoriesListDialog } from './CategoriesListDialog';
 
 describe('<CategoriesListDialog />', () => {
@@ -7,8 +9,18 @@ describe('<CategoriesListDialog />', () => {
   test('Show categories list', () => {
     render(<CategoriesListDialog subCategories={subcategories} categoryName={category} />);
 
-    screen.getByText(/first category/i);
+    expect(screen.getByText(/first category/i)).toBeInTheDocument();
     expect(screen.queryByText(/first subcategory/i)).not.toBeInTheDocument();
     expect(screen.queryByText(/second subcategory/i)).not.toBeInTheDocument();
+  });
+
+  test('Click on category, then show subcategories list', async () => {
+    render(<CategoriesListDialog subCategories={subcategories} categoryName={category} />);
+
+    const categoryButton = screen.getByRole('button', { name: /first category/i });
+    await act(async () => userEvent.click(categoryButton));
+
+    expect(await screen.findByText(/first subcategory/i)).toBeInTheDocument();
+    expect(screen.getByText(/second subcategory/i)).toBeInTheDocument();
   });
 });

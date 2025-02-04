d7027bc13fa488c5bb7f6cf755b2f5e519cecb3d
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-04 08:42:11 -0600

    add test Show categories and subcategories fetched

--- src/components/templates/CategoriesDialog/ShowCategories.test.tsx
+++ src/components/templates/CategoriesDialog/ShowCategories.test.tsx
@@ -1,19 +1,42 @@
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import fetchMock from 'jest-fetch-mock';
+
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { userInitialState } from '../../UI/Account/Account.mocks';
+import { successfulResponseFetchCategories } from '../../UI/Records/Record.mocks';
 import { ShowCategories } from './ShowCategories';
 
 describe('<ShowCategories />', () => {
-  test('Show loading categories state', () => {
-    const updateAction = jest.fn();
-    const updateCategoryToDelete = jest.fn();
-    const updateCategoryToEdit = jest.fn();
+  const updateAction = jest.fn();
+  const updateCategoryToDelete = jest.fn();
+  const updateCategoryToEdit = jest.fn();
 
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    jest.clearAllMocks();
+  });
+
+  test('Show loading categories state', () => {
     renderWithProviders(
       <ShowCategories updateAction={updateAction} updateCategoryToDelete={updateCategoryToDelete} updateCategoryToEdit={updateCategoryToEdit} />,
       { preloadedState: { user: userInitialState } },
     );
 
     expect(screen.getByText('Cargando categorías')).toBeInTheDocument();
+  });
+
+  test('Show categories and subcategories fetched', async () => {
+    fetchMock.once(JSON.stringify(successfulResponseFetchCategories));
+    renderWithProviders(
+      <ShowCategories updateAction={updateAction} updateCategoryToDelete={updateCategoryToDelete} updateCategoryToEdit={updateCategoryToEdit} />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    expect(await screen.findByText(/food and drink/i)).toBeInTheDocument();
+    const button = screen.getByRole('button', { name: /food and drink/i });
+    await act(async () => userEvent.click(button));
+    expect(await screen.findByText(/restaurants/i)).toBeInTheDocument();
   });
 });

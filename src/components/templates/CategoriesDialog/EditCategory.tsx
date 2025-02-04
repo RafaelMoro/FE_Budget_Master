3b769bd1f37ff5ba74f1e93e1b687a4e2cfbec13
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-04 15:53:21 -0600

    add formik and fields into edit category

--- src/components/templates/CategoriesDialog/EditCategory.tsx
+++ src/components/templates/CategoriesDialog/EditCategory.tsx
@@ -1,7 +1,42 @@
+import { Formik, Field } from 'formik';
 import { EditCategoryProps } from './CategoryDialog.interface';
+import { LoadingSpinner } from '../../UI/LoadingSpinner';
+import {
+  CancelButton, FlexContainer, InputForm, PrimaryButton,
+} from '../../../styles';
 
-const EditCategory = ({ categoryToEdit }: EditCategoryProps) => (
-  <p>Formulario para editar categoria</p>
-);
+const EditCategory = ({ categoryToEdit }: EditCategoryProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars, @typescript-eslint/no-explicit-any
+  const handleSubmit = (values: any) => {};
+  const loading = false;
+  const disableSubmitButton = false;
+
+  return (
+    <Formik
+      initialValues={{ something: '' }}
+  // validationSchema={CreateAccountSchema}
+      onSubmit={(values) => handleSubmit(values)}
+      validateOnMount
+    >
+      {({ submitForm }) => (
+        <FlexContainer gap={3} flexDirection="column" alignItems="center">
+          <Field
+            component={InputForm}
+            name="categoryName"
+            type="text"
+            variant="standard"
+            label="Título de la categoría"
+          />
+          <CancelButton>
+            Cancelar
+          </CancelButton>
+          <PrimaryButton disabled={disableSubmitButton} variant="contained" onClick={submitForm} size="medium">
+            { loading ? (<LoadingSpinner />) : 'Editar' }
+          </PrimaryButton>
+        </FlexContainer>
+      )}
+    </Formik>
+  );
+};
 
 export { EditCategory };

9675e2addc4786f6625686dd48fa6d222bca38a5
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-05 10:22:04 -0600

    add styles to error section

---
+++ src/components/UI/ErrorSection/ErrorSection.styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { IconButton } from '@mui/material';
+import { appTheme } from '../../../styles/theme';
+import { AppColors } from '../../../styles';
+
+export const ErrorContainer = styled.section`
+  padding: ${appTheme.spacing(3)};;
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${AppColors.negativeLight};
+  color: ${AppColors.white};
+`;
+
+export const CloseIconButton = styled(IconButton)`
+  justify-self: end;
+  grid-row: 1 / 2;
+`;
--- src/components/UI/ErrorSection/ErrorSection.tsx
+++ src/components/UI/ErrorSection/ErrorSection.tsx
@@ -1,5 +1,25 @@
-const ErrorSection = () => (
-  <p>Error</p>
+import { Typography } from '@mui/material';
+import { AppIcon } from '../Icons';
+import { CloseIconButton, ErrorContainer } from './ErrorSection.styled';
+
+interface ErrorSectionProps {
+  title?: string;
+  hideIcon?: boolean;
+  description: string;
+  onClose: () => void;
+}
+
+const ErrorSection = ({
+  title, description, onClose, hideIcon,
+}: ErrorSectionProps) => (
+  <ErrorContainer>
+    <CloseIconButton aria-label="boton-cerrar-error" onClick={onClose}>
+      <AppIcon icon="Close" />
+    </CloseIconButton>
+    { !hideIcon && (<AppIcon icon="Error" size="4.5rem" />) }
+    { title && (<Typography variant="h4" align="center">{title}</Typography>) }
+    <Typography>{description}</Typography>
+  </ErrorContainer>
 );
 
 export { ErrorSection };

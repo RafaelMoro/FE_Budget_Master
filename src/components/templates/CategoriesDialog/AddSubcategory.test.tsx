7f62a8c70ba01ab05195aa38ad3d4ae445a55b83
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-04 20:12:26 -0600

    add test Given a user adding a subcategory, the function addSubcategory is called

--- src/components/templates/CategoriesDialog/AddSubcategory.test.tsx
+++ src/components/templates/CategoriesDialog/AddSubcategory.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { AddSubcategory } from './AddSubcategory';
 
 describe('AddSubcategory', () => {
@@ -8,5 +9,19 @@ describe('AddSubcategory', () => {
 
     expect(screen.getByRole('textbox', { name: /agregar subcategoría/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /agregar subcategoría/i })).toBeInTheDocument();
+  });
+
+  test('Given a user adding a subcategory, the function addSubcategory is called', async () => {
+    const addSubcategory = jest.fn();
+    const newSubcategory = 'new subcategory';
+    render(<AddSubcategory addSubcategory={addSubcategory} />);
+
+    const addSubcategoryInput = screen.getByRole('textbox', { name: /agregar subcategoría/i });
+    const addSubcategoryButton = screen.getByRole('button', { name: /agregar subcategoría/i });
+
+    await act(async () => userEvent.type(addSubcategoryInput, newSubcategory));
+    await act(async () => userEvent.click(addSubcategoryButton));
+
+    expect(addSubcategory).toHaveBeenCalledWith(newSubcategory);
   });
 });

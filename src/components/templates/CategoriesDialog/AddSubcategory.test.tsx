4bb1416c495e5800a8ca08efe9602f2a7cf1c07c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-04 20:16:52 -0600

    add test Given a user adding a subcategory with more than 20 characters, the error message is shown

--- src/components/templates/CategoriesDialog/AddSubcategory.test.tsx
+++ src/components/templates/CategoriesDialog/AddSubcategory.test.tsx
@@ -35,6 +35,20 @@ describe('AddSubcategory', () => {
     expect(await screen.findByText(/por favor, ingrese una subcategoría de más de 3 caracteres/i)).toBeInTheDocument();
   });
 
+  test('Given a user adding a subcategory with more than 20 characters, the error message is shown', async () => {
+    const addSubcategory = jest.fn();
+    const newSubcategory = 'a subcategory with more than 20 characters';
+    render(<AddSubcategory addSubcategory={addSubcategory} />);
+
+    const addSubcategoryInput = screen.getByRole('textbox', { name: /agregar subcategoría/i });
+    const addSubcategoryButton = screen.getByRole('button', { name: /agregar subcategoría/i });
+
+    await act(async () => userEvent.type(addSubcategoryInput, newSubcategory));
+    await act(async () => userEvent.click(addSubcategoryButton));
+
+    expect(await screen.findByText(/por favor, ingrese una subcategoría con menos de 20 caracteres/i)).toBeInTheDocument();
+  });
+
   test('Given a user adding a subcategory, the function addSubcategory is called', async () => {
     const addSubcategory = jest.fn();
     const newSubcategory = 'new subcategory';

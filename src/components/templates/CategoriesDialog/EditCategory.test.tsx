3413db200347bba4624d186a135425d60f8a0747
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-06 10:14:06 -0600

    add test Given a user leaving empty the category name, show error message

--- src/components/templates/CategoriesDialog/EditCategory.test.tsx
+++ src/components/templates/CategoriesDialog/EditCategory.test.tsx
@@ -1,4 +1,6 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { CategoryUI } from '../../../globalInterface';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { userInitialState } from '../../UI/Account/Account.mocks';
@@ -32,5 +34,22 @@ describe('EditCategory', () => {
     expect(screen.getByRole('button', { name: /groceries/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /cancelar/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /editar/i })).toBeInTheDocument();
+  });
+
+  test('Given a user leaving empty the category name, show error message', async () => {
+    renderWithProviders(
+      <EditCategory categoryToEdit={categoryToEdit} goBackAction={goBackAction} updateError={updateError} />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    const categoryNameInput = screen.getByRole('textbox', { name: /título de la categoría/i });
+    const submitButton = screen.getByRole('button', { name: /editar/i });
+    await act(async () => userEvent.type(categoryNameInput, '{selectall}{backspace}'));
+    await waitFor(() => {
+      expect(categoryNameInput).toHaveValue('');
+    });
+
+    await act(async () => userEvent.click(submitButton));
+    expect(await screen.findByText(/por favor, ingrese un nombre de categoría/i)).toBeInTheDocument();
   });
 });

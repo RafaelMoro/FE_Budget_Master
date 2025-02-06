762d1c08092838a4c68b8e433fea8c20d51cbaab
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-06 10:17:09 -0600

    add test Given a user giving a category name with less than 3 characters, show error message

--- src/components/templates/CategoriesDialog/EditCategory.test.tsx
+++ src/components/templates/CategoriesDialog/EditCategory.test.tsx
@@ -52,4 +52,22 @@ describe('EditCategory', () => {
     await act(async () => userEvent.click(submitButton));
     expect(await screen.findByText(/por favor, ingrese un nombre de categoría/i)).toBeInTheDocument();
   });
+
+  test('Given a user giving a category name with less than 3 characters, show error message', async () => {
+    renderWithProviders(
+      <EditCategory categoryToEdit={categoryToEdit} goBackAction={goBackAction} updateError={updateError} />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    const categoryNameInput = screen.getByRole('textbox', { name: /título de la categoría/i });
+    const submitButton = screen.getByRole('button', { name: /editar/i });
+    await act(async () => userEvent.type(categoryNameInput, '{selectall}{backspace}'));
+    await act(async () => userEvent.type(categoryNameInput, 'a'));
+    await waitFor(() => {
+      expect(categoryNameInput).toHaveValue('a');
+    });
+
+    await act(async () => userEvent.click(submitButton));
+    expect(await screen.findByText(/por favor, ingrese una categoría de más de 3 caracteres/i)).toBeInTheDocument();
+  });
 });

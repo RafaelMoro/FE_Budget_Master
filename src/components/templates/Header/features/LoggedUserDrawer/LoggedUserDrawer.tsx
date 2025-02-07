dd3127fb61b39b19a6d55031d5e9b3b9fb5743fb
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-07 09:39:17 -0600

    add menu to logged user drawer

--- src/components/templates/Header/features/LoggedUserDrawer/LoggedUserDrawer.tsx
+++ src/components/templates/Header/features/LoggedUserDrawer/LoggedUserDrawer.tsx
@@ -1,11 +1,18 @@
-import { Divider, Drawer, Typography } from '@mui/material';
+import {
+  Divider, Drawer, ListItemText, Typography, List, ListItemButton,
+  Collapse,
+} from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+
+import { useState } from 'react';
+import { BUDGETS_ROUTE, DASHBOARD_ROUTE } from '../../../../../pages/RoutesConstants';
+import { ICON_SIZE } from '../../../../../constants';
 import { AppIcon } from '../../../../UI/Icons';
 import {
   ActiveDrawerMenuLink,
   CloseIconButton, DrawerMenu,
 } from '../../Header.styled';
 import { TransparentButton } from '../../../../../styles';
-import { BUDGETS_ROUTE, DASHBOARD_ROUTE } from '../../../../../pages/RoutesConstants';
 
 interface LoggedUserDrawerProps {
   open: boolean;
@@ -17,24 +24,42 @@ interface LoggedUserDrawerProps {
 
 const LoggedUserDrawer = ({
   open, activeBudgetsPage, activeDashboardPage, toggleDrawer, signOut,
-}: LoggedUserDrawerProps) => (
-  <Drawer anchor="bottom" open={open}>
-    <DrawerMenu>
-      <CloseIconButton onClick={toggleDrawer}>
-        <AppIcon icon="Close" />
-      </CloseIconButton>
-      <ActiveDrawerMenuLink active={activeDashboardPage} to={DASHBOARD_ROUTE}>
-        <Typography>Accounts</Typography>
-      </ActiveDrawerMenuLink>
-      <ActiveDrawerMenuLink active={activeBudgetsPage} to={BUDGETS_ROUTE}>
-        <Typography>Budgets</Typography>
-      </ActiveDrawerMenuLink>
-      <Divider style={{ width: '100%' }} />
-      <TransparentButton data-testid="log-out-button" onClick={signOut}>
-        Log out
-      </TransparentButton>
-    </DrawerMenu>
-  </Drawer>
-);
+}: LoggedUserDrawerProps) => {
+  const [openList, setOpenList] = useState(false);
+  const handleClick = () => setOpenList((prevState) => !prevState);
+  return (
+    <Drawer anchor="bottom" open={open}>
+      <DrawerMenu>
+        <CloseIconButton onClick={toggleDrawer}>
+          <AppIcon icon="Close" />
+        </CloseIconButton>
+        <List
+          sx={{
+            width: '100%', maxWidth: 360, bgcolor: 'background.paper', justifySelf: 'center',
+          }}
+          component="nav"
+          aria-labelledby="nested-list-categories"
+        >
+          <ListItemButton onClick={handleClick}>
+            <ListItemText primary="Navegación" />
+            {openList ? <ExpandLess sx={ICON_SIZE} /> : <ExpandMore sx={ICON_SIZE} />}
+          </ListItemButton>
+          <Collapse in={openList} timeout="auto" unmountOnExit>
+            <ActiveDrawerMenuLink active={activeDashboardPage} to={DASHBOARD_ROUTE}>
+              <Typography>Accounts</Typography>
+            </ActiveDrawerMenuLink>
+            <ActiveDrawerMenuLink active={activeBudgetsPage} to={BUDGETS_ROUTE}>
+              <Typography>Budgets</Typography>
+            </ActiveDrawerMenuLink>
+          </Collapse>
+        </List>
+        <Divider style={{ width: '100%' }} />
+        <TransparentButton data-testid="log-out-button" onClick={signOut}>
+          Log out
+        </TransparentButton>
+      </DrawerMenu>
+    </Drawer>
+  );
+};
 
 export { LoggedUserDrawer };

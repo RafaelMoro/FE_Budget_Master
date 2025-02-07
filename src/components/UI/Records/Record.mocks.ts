551c0fbd0e1265b239c4e32b5b4cda1fd1d52a57
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-07 12:43:56 -0600

    add test Given a user editing a category and fails the edition, show the appropiate error message

--- src/components/UI/Records/Record.mocks.ts
+++ src/components/UI/Records/Record.mocks.ts
@@ -358,6 +358,16 @@ export const successfulCreateEditCategoriesReponse = {
   success: true,
   version: '2.0.0',
 };
+export const failedCreateEditCategoriesReponse = {
+  data: null,
+  error: {
+    statusCode: 401,
+    message: 'Unauthorized',
+  },
+  message: null,
+  success: false,
+  version: '2.0.0',
+};
 export const emptyCategoriesResponse = {
   data: {
     categories: [],
--- src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
@@ -5,11 +5,13 @@ import fetchMock from 'jest-fetch-mock';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { CategoriesDialog } from './CategoriesDialog';
 import { userInitialState } from '../../UI/Account/Account.mocks';
-import { ERROR_MESSAGE_FETCH_CATEGORY, ERROR_MESSAGE_GENERAL } from '../../../constants';
-import { failedResponseFetchCategories, successfulResponseFetchCategories } from '../../UI/Records/Record.mocks';
+import { ERROR_MESSAGE_EDIT_CATEGORY, ERROR_MESSAGE_FETCH_CATEGORY, ERROR_MESSAGE_GENERAL } from '../../../constants';
+import { failedCreateEditCategoriesReponse, failedResponseFetchCategories, successfulResponseFetchCategories } from '../../UI/Records/Record.mocks';
 
 describe('<CategoriesDialog />', () => {
   const onClose = jest.fn();
+
+  const newSubcategory = 'new subcategory 1';
 
   beforeEach(() => {
     fetchMock.resetMocks();
@@ -64,6 +66,39 @@ describe('<CategoriesDialog />', () => {
       /puede cambiar el nombre de la categoría, agregar subcategoría, o bien, eliminar una subcategoría dando click en el botón en forma de x que está junto a la subcategoría/i,
     );
     expect(description).toBeInTheDocument();
+  });
+
+  test('Given a user editing a category and fails the edition, show the appropiate error message', async () => {
+    fetchMock.once(JSON.stringify(successfulResponseFetchCategories));
+    fetchMock.mockRejectedValueOnce(JSON.stringify(failedCreateEditCategoriesReponse));
+    renderWithProviders(
+      <CategoriesDialog onClose={onClose} open />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    expect(await screen.findByRole('button', { name: /Food and Drink/i })).toBeInTheDocument();
+    const categoryButton = screen.getByRole('button', { name: /Food and Drink/i });
+    await act(async () => userEvent.click(categoryButton));
+
+    expect(await screen.findByRole('button', { name: /boton-editar-categoria-food and drink/i }));
+    const editCategoryButton = screen.getByRole('button', { name: /boton-editar-categoria-food and drink/i });
+    await act(async () => userEvent.click(editCategoryButton));
+
+    expect(await screen.findByRole('heading', { name: /editar categoría/i })).toBeInTheDocument();
+
+    const categoryNameInput = screen.getByRole('textbox', { name: /título de la categoría/i });
+    const submitButton = screen.getByRole('button', { name: /editar/i });
+    const subcategoryInput = screen.getByRole('textbox', { name: /subcategoría$/i });
+    const addSubcategoryButton = screen.getByRole('button', { name: /agregar subcategoría/i });
+
+    await act(async () => userEvent.type(categoryNameInput, ' 2'));
+    await act(async () => userEvent.type(subcategoryInput, newSubcategory));
+    await act(async () => userEvent.click(addSubcategoryButton));
+    await act(async () => userEvent.click(submitButton));
+
+    expect(await screen.findByRole('heading', { name: /categorías/i })).toBeInTheDocument();
+    expect(screen.getByText(ERROR_MESSAGE_EDIT_CATEGORY)).toBeInTheDocument();
+    expect(screen.getByText(ERROR_MESSAGE_GENERAL)).toBeInTheDocument();
   });
 
   test('Given a user deleting a category, show the appropiate title and description', async () => {

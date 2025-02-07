182b31d910258e041b17e5391ee43c1471cc0dfd
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-07 12:23:21 -0600

    add test Given an error while fetching categories, show error message

--- src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
@@ -4,6 +4,8 @@ import fetchMock from 'jest-fetch-mock';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
 import { CategoriesDialog } from './CategoriesDialog';
 import { userInitialState } from '../../UI/Account/Account.mocks';
+import { ERROR_MESSAGE_FETCH_CATEGORY, ERROR_MESSAGE_GENERAL } from '../../../constants';
+import { failedResponseFetchCategories } from '../../UI/Records/Record.mocks';
 
 describe('<CategoriesDialog />', () => {
   const onClose = jest.fn();
@@ -23,5 +25,16 @@ describe('<CategoriesDialog />', () => {
     // eslint-disable-next-line max-len
     const description = 'Haga click en cualquier categoría para ver sus subcategorías. Dentro encontrará los botones para editar o eliminar esa categoría.';
     expect(screen.getByText(description)).toBeInTheDocument();
+  });
+
+  test('Given an error while fetching categories, show error message', async () => {
+    fetchMock.mockRejectedValueOnce(JSON.stringify(failedResponseFetchCategories));
+    renderWithProviders(
+      <CategoriesDialog onClose={onClose} open />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    expect(await screen.findByText(ERROR_MESSAGE_FETCH_CATEGORY)).toBeInTheDocument();
+    expect(screen.getByText(ERROR_MESSAGE_GENERAL)).toBeInTheDocument();
   });
 });

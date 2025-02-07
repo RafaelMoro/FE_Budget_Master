0146446ccae78fde93096ee34164b8f4c1ddb241
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-02-06 18:51:49 -0600

    add select category icon

--- src/components/templates/CategoriesDialog/SelectCategoryIcon.tsx
+++ src/components/templates/CategoriesDialog/SelectCategoryIcon.tsx
@@ -1,11 +1,35 @@
+import {
+  RadioGroup, Radio, FormControlLabel, FormControlLabelProps, useRadioGroup,
+} from '@mui/material';
 import { ICONS_MATERIAL } from '../../UI/Icons/Icons.constants';
 import { SelectIcon } from '../../UI/Icons/SelectIcon';
 
+function MyFormControlLabel(props: FormControlLabelProps) {
+  const radioGroup = useRadioGroup();
+
+  let checked = false;
+
+  if (radioGroup) {
+    // eslint-disable-next-line react/destructuring-assignment
+    checked = radioGroup.value === props.value;
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <FormControlLabel checked={checked} {...props} />;
+}
+
 const SelectCategoryIcon = () => (
   <div>
-    { Object.keys(ICONS_MATERIAL).map((iconName) => (
-      <SelectIcon key={iconName} name={iconName} />
-    )) }
+    <RadioGroup name="use-radio-group" defaultValue="first">
+      { Object.keys(ICONS_MATERIAL).map((iconName) => (
+        <MyFormControlLabel
+          key={iconName}
+          value={iconName}
+          label={<SelectIcon name={iconName} />}
+          control={<Radio />}
+        />
+      )) }
+    </RadioGroup>
   </div>
 );
 

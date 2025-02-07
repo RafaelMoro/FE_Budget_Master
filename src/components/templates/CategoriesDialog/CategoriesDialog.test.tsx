870ad0b6a55c914ce13f525484d2a57d411340f7
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-07 12:26:17 -0600

    add test Given a user creating a category, show the appropiate title and description

--- src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
+++ src/components/templates/CategoriesDialog/CategoriesDialog.test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import fetchMock from 'jest-fetch-mock';
 import { renderWithProviders } from '../../../tests/CustomWrapperRedux';
@@ -25,6 +26,21 @@ describe('<CategoriesDialog />', () => {
     // eslint-disable-next-line max-len
     const description = 'Haga click en cualquier categoría para ver sus subcategorías. Dentro encontrará los botones para editar o eliminar esa categoría.';
     expect(screen.getByText(description)).toBeInTheDocument();
+  });
+
+  test('Given a user creating a category, show the appropiate title and description', async () => {
+    renderWithProviders(
+      <CategoriesDialog onClose={onClose} open />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    const createCategoryButton = screen.getByRole('button', { name: /crear categoría/i });
+    await act(async () => userEvent.click(createCategoryButton));
+
+    expect(await screen.findByRole('heading', { name: /crear categoría/i })).toBeInTheDocument();
+    expect(
+      screen.getByText(/puede crear una nueva categoría, ingresando su nombre y seleccionando las subcategorías que desea agregar\./i),
+    ).toBeInTheDocument();
   });
 
   test('Given an error while fetching categories, show error message', async () => {

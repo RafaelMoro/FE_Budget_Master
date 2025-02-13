e0c5f847c07d33c529c9a3602b4721aa2cabb273
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-13 08:49:58 -0600

    add helmet provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { HelmetProvider } from 'react-helmet-async';
 
 import App from './App';
 
@@ -8,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );

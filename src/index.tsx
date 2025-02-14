7443792c0f3bfe142ea615f8607163fd23033e42
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-13 20:05:42 -0600

    remove eslint suppresision

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { HelmetProvider } from 'react-helmet-async';
 
 import App from './App';
